e0ae875eec8a2cca70568ecf595be39f5d34f20b
Felipe Uenes <uenesdesigner@gmail.com>
2023-02-28 09:57:38 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import imageLuana from './images/header.png'
+
+
+export function App() {
+ 
+
+  return (
+    <div className="container">
+      <img src={imageLuana} alt="" srcset="" />
+    </div>
+  )
+}
+
