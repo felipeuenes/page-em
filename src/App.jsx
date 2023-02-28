602376b12c2591db0606393ead38554f1b208492
Felipe Uenes <uenesdesigner@gmail.com>
2023-02-28 10:08:55 -0300

    add list

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,25 @@ export function App() {
   return (
     <div className="container">
       <img src={imageLuana} alt="" srcset="" />
+
+      <div className='servicos'>
+        <ul>
+          <li>Orientações personalizadas.</li>
+          <li>Visitas de acompanhamento in loco.</li>
+          <li>Capacitações estratégicas.</li>
+          <li>Orientações para o crédito responsável.</li>
+          <li>Orientação para abertura e baixa de MEI.</li>
+          <li>Eventos de oportunidade de negócio.</li>
+          <li>Intercâmbio e network.</li>
+          <li>Parcerias.</li>
+          <li>Atendimento semanal de serviços contábeis
+para MEI e orientações.</li>
+          <li>Oportunizamos acesso a vagas no mercado
+de trabalho.</li>
+        </ul>
+      </div>
+
+
     </div>
   )
 }
