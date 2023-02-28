136df2f0fb0c1ad71d22e55f9228d2d3cf112860
Felipe Uenes <uenesdesigner@gmail.com>
2023-02-28 12:47:09 -0300

    add map

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,13 @@ de trabalho.</li>
 
       </div>
 
+      <h1>ONDE ESTAMOS?</h1>
+      <div>
+
+
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d991.0004561209611!2d-39.52280767559664!3d-6.521450077744051!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7a2f71cca748c23%3A0x7e5168671208db6!2sPolo%20Artesanal%20de%20Juc%C3%A1s!5e0!3m2!1spt-BR!2sbr!4v1677598889575!5m2!1spt-BR!2sbr"></iframe>
+
+      </div>
 
     </div>
   )
